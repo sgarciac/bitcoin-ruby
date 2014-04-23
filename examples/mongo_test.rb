fe44ade1f18ba36d20db91a734dfd6596a39c1dd
sergio <sergio.garcia@gmail.com>
2014-04-23 15:59:53 -0400

    adding example comment

--- examples/mongo_test.rb
+++ examples/mongo_test.rb
@@ -5,4 +5,5 @@ $:.unshift( File.expand_path("../../lib", __FILE__) )
 require 'bitcoin'
 
 Bitcoin.network = :bitcoin
+#use a default url
 store = Bitcoin::Storage.mongo(:db => "mongodb://localhost/bitcoin")
